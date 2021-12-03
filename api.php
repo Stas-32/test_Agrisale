411613ab6dd028c7093592632025c0c1135dd8a4
Ra[32] <87080595+Stas-32@users.noreply.github.com>
2021-12-03 14:00:57 +0300

    Add files

---
+++ api.php
@@ -0,0 +1,66 @@
+<?php
+
+	$array = array(
+		array("seller" => "ООО НПО «АгроСоРос»", "region" => "Все регионы", "rating" => "5.00", "option" => 1, "count" => 150000, "price" => 12286.01, "currency" => "RUB", "sellerID" => 1),
+		array("seller" => "ООО «Синегта»", "region" => "Все регионы", "rating" => "5.00", "option" => 0, "count" => 120000, "price" => 10286.00, "currency" => "RUB", "sellerID" => 2),
+		array("seller" => "ООО «АгроЛидер»", "region" => "Все регионы", "rating" => "5.00", "option" => 1, "count" => 170000, "price" => 177.50, "currency" => "USD", "sellerID" => 3),
+		array("seller" => "ООО НПО «АгроСоРос»", "region" => "Все регионы", "rating" => "4.78", "option" => 1, "count" => 150000, "price" => 12286.01, "currency" => "RUB", "sellerID" => 4),
+		array("seller" => "ООО НПО «АгроСоРос»", "region" => "Все регионы", "rating" => "5.00", "option" => 2, "count" => 150000, "price" => 12286.01, "currency" => "RUB", "sellerID" => 5),
+		array("seller" => "ООО НПО «АгроСоРос»", "region" => "Все регионы", "rating" => "5.00", "option" => 1, "count" => 150000, "price" => 12286.01, "currency" => "RUB", "sellerID" => 6),
+		array("seller" => "ООО «Синегта»", "region" => "Все регионы", "rating" => "5.00", "option" => 0, "count" => 120000, "price" => 10286.00, "currency" => "RUB", "sellerID" => 7),
+		array("seller" => "ООО «АгроЛидер»", "region" => "Все регионы", "rating" => "5.00", "option" => 1, "count" => 170000, "price" => 177.50, "currency" => "USD", "sellerID" => 8),
+		array("seller" => "ООО НПО «АгроСоРос»", "region" => "Все регионы", "rating" => "4.78", "option" => 1, "count" => 150000, "price" => 12286.01, "currency" => "RUB", "sellerID" => 9),
+		array("seller" => "ООО НПО «АгроСоРос»", "region" => "Все регионы", "rating" => "5.00", "option" => 1, "count" => 150000, "price" => 12286.01, "currency" => "RUB", "sellerID" => 10),
+		array("seller" => "ООО «Синегта»", "region" => "Все регионы", "rating" => "5.00", "option" => 0, "count" => 120000, "price" => 10286.00, "currency" => "RUB", "sellerID" => 11),
+		array("seller" => "ООО «АгроЛидер»", "region" => "Все регионы", "rating" => "5.00", "option" => 1, "count" => 170000, "price" => 177.50, "currency" => "USD", "sellerID" => 12),
+		array("seller" => "ООО НПО «АгроСоРос»", "region" => "Все регионы", "rating" => "4.78", "option" => 1, "count" => 150000, "price" => 12286.01, "currency" => "RUB", "sellerID" => 13),
+		array("seller" => "ООО НПО «АгроСоРос»", "region" => "Все регионы", "rating" => "5.00", "option" => 1, "count" => 150000, "price" => 12286.01, "currency" => "RUB", "sellerID" => 14),
+		array("seller" => "ООО «Синегта»", "region" => "Все регионы", "rating" => "5.00", "option" => 0, "count" => 120000, "price" => 10286.00, "currency" => "RUB", "sellerID" => 15),
+		array("seller" => "ООО «АгроЛидер»", "region" => "Все регионы", "rating" => "5.00", "option" => 1, "count" => 170000, "price" => 177.50, "currency" => "USD", "sellerID" => 16),
+		array("seller" => "ООО НПО «АгроСоРос»", "region" => "Все регионы", "rating" => "4.78", "option" => 1, "count" => 150000, "price" => 12286.01, "currency" => "RUB", "sellerID" => 17),
+		array("seller" => "ООО НПО «АгроСоРос»", "region" => "Все регионы", "rating" => "5.00", "option" => 1, "count" => 150000, "price" => 12286.01, "currency" => "RUB", "sellerID" => 18),
+		array("seller" => "ООО «Синегта»", "region" => "Все регионы", "rating" => "5.00", "option" => 0, "count" => 120000, "price" => 10286.00, "currency" => "RUB", "sellerID" => 19),
+		array("seller" => "ООО «АгроЛидер»", "region" => "Все регионы", "rating" => "5.00", "option" => 1, "count" => 170000, "price" => 177.50, "currency" => "USD", "sellerID" => 20),
+		array("seller" => "ООО НПО «АгроСоРос»", "region" => "Все регионы", "rating" => "4.78", "option" => 1, "count" => 150000, "price" => 12286.01, "currency" => "RUB", "sellerID" => 21),
+		array("seller" => "ООО НПО «АгроСоРос»", "region" => "Все регионы", "rating" => "5.00", "option" => 1, "count" => 150000, "price" => 12286.01, "currency" => "RUB", "sellerID" => 22),
+		array("seller" => "ООО «Синегта»", "region" => "Все регионы", "rating" => "5.00", "option" => 0, "count" => 120000, "price" => 10286.00, "currency" => "RUB", "sellerID" => 23),
+		array("seller" => "ООО «АгроЛидер»", "region" => "Все регионы", "rating" => "5.00", "option" => 1, "count" => 170000, "price" => 177.50, "currency" => "USD", "sellerID" => 24),
+		array("seller" => "ООО НПО «АгроСоРос»", "region" => "Все регионы", "rating" => "4.78", "option" => 1, "count" => 150000, "price" => 12286.01, "currency" => "RUB", "sellerID" => 25),
+		array("seller" => "ООО НПО «АгроСоРос»", "region" => "Все регионы", "rating" => "5.00", "option" => 1, "count" => 150000, "price" => 12286.01, "currency" => "RUB", "sellerID" => 26),
+		array("seller" => "ООО «Синегта»", "region" => "Все регионы", "rating" => "5.00", "option" => 0, "count" => 120000, "price" => 10286.00, "currency" => "RUB", "sellerID" => 27),
+		array("seller" => "ООО «АгроЛидер»", "region" => "Все регионы", "rating" => "5.00", "option" => 1, "count" => 170000, "price" => 177.50, "currency" => "USD", "sellerID" => 28),
+		array("seller" => "ООО НПО «АгроСоРос»", "region" => "Все регионы", "rating" => "4.78", "option" => 1, "count" => 150000, "price" => 12286.01, "currency" => "RUB", "sellerID" => 29),
+		array("seller" => "ООО НПО «АгроСоРос»", "region" => "Все регионы", "rating" => "5.00", "option" => 1, "count" => 150000, "price" => 12286.01, "currency" => "RUB", "sellerID" => 30),
+		array("seller" => "ООО «Синегта»", "region" => "Все регионы", "rating" => "5.00", "option" => 0, "count" => 120000, "price" => 10286.00, "currency" => "RUB", "sellerID" => 31),
+		array("seller" => "ООО «АгроЛидер»", "region" => "Все регионы", "rating" => "5.00", "option" => 1, "count" => 170000, "price" => 177.50, "currency" => "USD", "sellerID" => 32),
+		array("seller" => "ООО НПО «АгроСоРос»", "region" => "Все регионы", "rating" => "4.78", "option" => 1, "count" => 150000, "price" => 12286.01, "currency" => "RUB", "sellerID" => 33),
+		array("seller" => "ООО НПО «АгроСоРос»", "region" => "Все регионы", "rating" => "5.00", "option" => 1, "count" => 150000, "price" => 12286.01, "currency" => "RUB", "sellerID" => 34),
+		array("seller" => "ООО «Синегта»", "region" => "Все регионы", "rating" => "5.00", "option" => 0, "count" => 120000, "price" => 10286.00, "currency" => "RUB", "sellerID" => 35),
+		array("seller" => "ООО «АгроЛидер»", "region" => "Все регионы", "rating" => "5.00", "option" => 1, "count" => 170000, "price" => 177.50, "currency" => "USD", "sellerID" => 36),
+		array("seller" => "ООО НПО «АгроСоРос»", "region" => "Все регионы", "rating" => "4.78", "option" => 1, "count" => 150000, "price" => 12286.01, "currency" => "RUB", "sellerID" => 37),
+		array("seller" => "ООО НПО «АгроСоРос»", "region" => "Все регионы", "rating" => "5.00", "option" => 1, "count" => 150000, "price" => 12286.01, "currency" => "RUB", "sellerID" => 38),
+		array("seller" => "ООО «Синегта»", "region" => "Все регионы", "rating" => "5.00", "option" => 0, "count" => 120000, "price" => 10286.00, "currency" => "RUB", "sellerID" => 39),
+		array("seller" => "ООО «АгроЛидер»", "region" => "Все регионы", "rating" => "5.00", "option" => 1, "count" => 170000, "price" => 177.50, "currency" => "USD", "sellerID" => 40),
+		array("seller" => "ООО НПО «АгроСоРос»", "region" => "Все регионы", "rating" => "4.78", "option" => 1, "count" => 150000, "price" => 12286.01, "currency" => "RUB", "sellerID" => 41),
+		array("seller" => "ООО НПО «АгроСоРос»", "region" => "Все регионы", "rating" => "5.00", "option" => 1, "count" => 150000, "price" => 12286.01, "currency" => "RUB", "sellerID" => 42),
+		array("seller" => "ООО «Синегта»", "region" => "Все регионы", "rating" => "5.00", "option" => 0, "count" => 120000, "price" => 10286.00, "currency" => "RUB", "sellerID" => 43),
+		array("seller" => "ООО «АгроЛидер»", "region" => "Все регионы", "rating" => "5.00", "option" => 1, "count" => 170000, "price" => 177.50, "currency" => "USD", "sellerID" => 44),
+		array("seller" => "ООО НПО «АгроСоРос»", "region" => "Все регионы", "rating" => "4.78", "option" => 1, "count" => 150000, "price" => 12286.01, "currency" => "RUB", "sellerID" => 45),
+	);
+
+	$method = $_GET["method"];
+
+	if($method) {
+		switch($method) {
+			case 'getTableData':
+				echo json_encode($array);
+			break;
+			default:
+				echo 0;
+			break;
+		}
+	} else {
+		echo 0;
+	}
+
+?>
